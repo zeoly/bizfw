9f4ed5e0aae8cdb2716c4b280981c4c1ae21bf68
zeoly <zeoly100@163.com>
2017-05-08 14:27:39 +0800

    exam detail model

---
+++ src/bizfw/src/main/java/com/thinkequip/bizfw/exam/model/AnswerSheetDetail.java
@@ -0,0 +1,93 @@
+package com.thinkequip.bizfw.exam.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.thinkequip.bizfw.base.model.BaseModel;
+
+/**
+ * 答题卡详情模型
+ * 
+ * @copyright THINKEQUIP
+ * @author zengyongli
+ * @date 2017年5月8日
+ */
+@Entity
+@Table(name = "bf_an_sheet_detail")
+public class AnswerSheetDetail extends BaseModel {
+
+	private static final long serialVersionUID = -3341663104034113338L;
+
+	/** 主键 */
+	@Id
+	@Column(name = "id_bf_an_sheet_detail")
+	@GenericGenerator(name = "uuid", strategy = "uuid")
+	@GeneratedValue(generator = "uuid")
+	private String idBfAnswerSheetDetail;
+
+	/** 对应答题卡id */
+	@Column(name = "id_bf_answer_sheet")
+	private String AnswerSheetId;
+
+	/** 对应试卷问题id */
+	@Column(name = "id_bf_paper_question")
+	private String paperQuestionId;
+
+	/** 答题卡详情答案 */
+	@Column(name = "sheet_answer")
+	private String sheetAnswer;
+
+	/** 答题卡问题得分 */
+	@Column(name = "score")
+	private Integer score;
+
+	public AnswerSheetDetail(String peopleCode) {
+		super(peopleCode);
+	}
+
+	public String getIdBfAnswerSheetDetail() {
+		return idBfAnswerSheetDetail;
+	}
+
+	public void setIdBfAnswerSheetDetail(String idBfAnswerSheetDetail) {
+		this.idBfAnswerSheetDetail = idBfAnswerSheetDetail;
+	}
+
+	public String getAnswerSheetId() {
+		return AnswerSheetId;
+	}
+
+	public void setAnswerSheetId(String answerSheetId) {
+		AnswerSheetId = answerSheetId;
+	}
+
+	public String getPaperQuestionId() {
+		return paperQuestionId;
+	}
+
+	public void setPaperQuestionId(String paperQuestionId) {
+		this.paperQuestionId = paperQuestionId;
+	}
+
+	public String getSheetAnswer() {
+		return sheetAnswer;
+	}
+
+	public void setSheetAnswer(String sheetAnswer) {
+		this.sheetAnswer = sheetAnswer;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+}
---
+++ src/bizfw/src/main/java/com/thinkequip/bizfw/exam/model/PaperQuestion.java
@@ -0,0 +1,117 @@
+package com.thinkequip.bizfw.exam.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.thinkequip.bizfw.base.model.BaseModel;
+
+/**
+ * 试卷问题
+ * 
+ * @copyright THINKEQUIP
+ * @author zengyongli
+ * @date 2017年5月8日
+ */
+@Entity
+@Table(name = "bf_paper_question")
+public class PaperQuestion extends BaseModel {
+
+	private static final long serialVersionUID = 1989735737484878248L;
+
+	/** 主键 */
+	@Id
+	@Column(name = "id_bf_paper_question")
+	@GenericGenerator(name = "uuid", strategy = "uuid")
+	@GeneratedValue(generator = "uuid")
+	private String idBfPaperQuestion;
+
+	/** 对应试卷id */
+	@Column(name = "id_bf_exam_paper")
+	private String examinationPaperId;
+
+	/** 试题编号 */
+	@Column(name = "orders")
+	private Integer orders;
+
+	/** 对应问题id */
+	@Column(name = "id_bf_question")
+	private String questionId;
+
+	/** 试题体 */
+	@Column(name = "question_body")
+	private String questionBody;
+
+	/** 试题答案 */
+	@Column(name = "answer")
+	private String answer;
+
+	/** 试题分数 */
+	@Column(name = "score")
+	private Integer score;
+
+	public PaperQuestion(String peopleCode) {
+		super(peopleCode);
+	}
+
+	public String getIdBfPaperQuestion() {
+		return idBfPaperQuestion;
+	}
+
+	public void setIdBfPaperQuestion(String idBfPaperQuestion) {
+		this.idBfPaperQuestion = idBfPaperQuestion;
+	}
+
+	public String getExaminationPaperId() {
+		return examinationPaperId;
+	}
+
+	public void setExaminationPaperId(String examinationPaperId) {
+		this.examinationPaperId = examinationPaperId;
+	}
+
+	public Integer getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Integer orders) {
+		this.orders = orders;
+	}
+
+	public String getQuestionId() {
+		return questionId;
+	}
+
+	public void setQuestionId(String questionId) {
+		this.questionId = questionId;
+	}
+
+	public String getQuestionBody() {
+		return questionBody;
+	}
+
+	public void setQuestionBody(String questionBody) {
+		this.questionBody = questionBody;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+}
