206d26af91cb8ffbf7e85547d5151d151a475013
zeoly <zeoly100@163.com>
2017-05-08 15:14:17 +0800

    exam question lib DAO

---
+++ src/bizfw/src/main/java/com/thinkequip/bizfw/exam/dao/SubjectDao.java
@@ -0,0 +1,15 @@
+package com.thinkequip.bizfw.exam.dao;
+
+import com.thinkequip.bizfw.base.BaseDao;
+import com.thinkequip.bizfw.exam.model.Subject;
+
+/**
+ * 考试科目dao
+ * 
+ * @copyright THINKEQUIP
+ * @author zengyongli
+ * @date 2017年5月8日
+ */
+public interface SubjectDao extends BaseDao<Subject> {
+
+}
---
+++ src/bizfw/src/main/java/com/thinkequip/bizfw/exam/impl/SubjectDaoImpl.java
@@ -0,0 +1,19 @@
+package com.thinkequip.bizfw.exam.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.thinkequip.bizfw.base.impl.BaseDaoImpl;
+import com.thinkequip.bizfw.exam.dao.SubjectDao;
+import com.thinkequip.bizfw.exam.model.Subject;
+
+/**
+ * 考试科目dao实现类
+ * 
+ * @copyright THINKEQUIP
+ * @author zengyongli
+ * @date 2017年5月8日
+ */
+@Repository
+public class SubjectDaoImpl extends BaseDaoImpl<Subject> implements SubjectDao {
+
+}
