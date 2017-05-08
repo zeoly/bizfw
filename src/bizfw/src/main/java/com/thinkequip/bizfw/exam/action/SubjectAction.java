5d1efd829a0bdb2bbb7bde7ff956d465f3af6b17
zeoly <zeoly100@163.com>
2017-05-08 16:32:09 +0800

    exam subject module

---
+++ src/bizfw/src/main/java/com/thinkequip/bizfw/exam/action/SubjectAction.java
@@ -0,0 +1,48 @@
+package com.thinkequip.bizfw.exam.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.thinkequip.bizfw.base.BaseAction;
+import com.thinkequip.bizfw.base.BizfwServiceException;
+import com.thinkequip.bizfw.exam.model.Subject;
+import com.thinkequip.bizfw.exam.service.SubjectService;
+
+/**
+ * 考试科目action
+ * 
+ * @copyright THINKEQUIP
+ * @author zengyongli
+ * @date 2017年5月8日
+ */
+@Controller
+@RequestMapping("/subjectAction")
+public class SubjectAction extends BaseAction {
+
+	@Autowired
+	private SubjectService subjectService;
+
+	@ResponseBody
+	@RequestMapping("/getAllSubject")
+	public List<Subject> getAllSubject() throws BizfwServiceException {
+		return subjectService.getAllSubject();
+	}
+
+	@ResponseBody
+	@RequestMapping("/addSubject.do")
+	public void addSubject(HttpServletRequest request, Subject subject) throws BizfwServiceException {
+
+	}
+
+	@ResponseBody
+	@RequestMapping("/deleteSubject.do")
+	public void deleteSubject(HttpServletRequest request, String subjectId) throws BizfwServiceException {
+
+	}
+}
