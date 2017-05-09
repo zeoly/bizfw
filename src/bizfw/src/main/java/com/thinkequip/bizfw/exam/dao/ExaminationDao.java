c01929f806ef5b05c684f6ca6ae87507e25c0e5d
zeoly <zeoly100@163.com>
2017-05-09 14:19:52 +0800

    exam module dao

---
+++ src/bizfw/src/main/java/com/thinkequip/bizfw/exam/dao/ExaminationDao.java
@@ -0,0 +1,15 @@
+package com.thinkequip.bizfw.exam.dao;
+
+import com.thinkequip.bizfw.base.BaseDao;
+import com.thinkequip.bizfw.exam.model.Examination;
+
+/**
+ * 考试dao
+ * 
+ * @copyright THINKEQUIP
+ * @author zengyongli
+ * @date 2017年5月9日
+ */
+public interface ExaminationDao extends BaseDao<Examination> {
+
+}
---
+++ src/bizfw/src/main/java/com/thinkequip/bizfw/exam/dao/ExaminationPaperDao.java
@@ -0,0 +1,15 @@
+package com.thinkequip.bizfw.exam.dao;
+
+import com.thinkequip.bizfw.base.BaseDao;
+import com.thinkequip.bizfw.exam.model.ExaminationPaper;
+
+/**
+ * 试卷dao
+ * 
+ * @copyright THINKEQUIP
+ * @author zengyongli
+ * @date 2017年5月9日
+ */
+public interface ExaminationPaperDao extends BaseDao<ExaminationPaper> {
+
+}
