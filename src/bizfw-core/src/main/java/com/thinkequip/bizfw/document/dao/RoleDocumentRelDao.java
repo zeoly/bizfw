f9fbaea9dcd879a43641d6b65daee4ee60b5466b
zeoly <zeoly100@163.com>
2017-05-15 11:23:46 +0800

    delete unused

--- src/bizfw-core/src/main/java/com/thinkequip/bizfw/document/dao/RoleDocumentRelDao.java
+++
@@ -1,9 +0,0 @@
-package com.thinkequip.bizfw.document.dao;
-
-import com.thinkequip.bizfw.base.BaseDao;
-import com.thinkequip.bizfw.document.model.RoleDocumentRelation;
-
-public interface RoleDocumentRelDao extends BaseDao<RoleDocumentRelation> {
-
-	public RoleDocumentRelation getRelation(String roleId, String documentId);
-}
