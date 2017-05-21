bc275d89fe48cb89c83af9172a12e37a9afecbe8
zeoly <zeoly100@163.com>
2017-05-21 22:26:51 +0800

    paper action

---
+++ src/thinkequip-exam/src/main/java/com/thinkequip/exam/action/ExaminationPaperAction.java
@@ -0,0 +1,32 @@
+package com.thinkequip.exam.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.thinkequip.bizfw.base.BaseAction;
+import com.thinkequip.bizfw.base.BizfwServiceException;
+import com.thinkequip.exam.model.ExaminationPaper;
+import com.thinkequip.exam.service.ExaminationPaperService;
+
+/**
+ * 试卷action
+ * 
+ * @copyright THINKEQUIP
+ * @author zengyongli
+ * @date 2017年5月21日
+ */
+@Controller
+@RequestMapping("/examinationPaperAction")
+public class ExaminationPaperAction extends BaseAction {
+
+	@Autowired
+	private ExaminationPaperService examinationPaperService;
+
+	@ResponseBody
+	@RequestMapping("/addPaper.do")
+	public void addPaper(ExaminationPaper examinationPaper) throws BizfwServiceException {
+
+	}
+}
